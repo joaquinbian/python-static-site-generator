31fef7e9b407b645b5c858a53c5ef85d79d3151f
joaquinbian <joacobian21@gmail.com>
2024-07-27 11:45:04 -0300

    text node class

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+
+def main():
+    print("hello world from python!!")
+    bold = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    bold2 = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(bold)
+
+main()
