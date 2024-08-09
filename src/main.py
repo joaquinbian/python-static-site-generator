d6aca5d82fb2f38df6d66fff2f37550eea44655a
joaquinbian <joacobian21@gmail.com>
2024-08-09 00:02:12 -0300

    split node delimiter

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 from textnode import TextNode, text_type_text, text_type_link, text_type_italic, text_type_image, text_type_code, text_type_bold
 from htmlnode import HTMLNode, LeafNode, ParentNode
-
+from split_node_delimiter import split_nodes_delimiter
 
 
 def main():
     print("hello world from python!!")
-    bold = TextNode("This is a text node", "bold")
-    bold2 = TextNode("This is a text node", "bold")
+    bold = TextNode("This is a *bold text node*", text_type_text)
+    code = TextNode("This is a `code` text `node`", text_type_text)
     node = HTMLNode("a", "hola mundo", None, {"href":"github.com", "target":"_blank"})
     leaf = LeafNode("b", "This is a BOLD text.")
     leaf3 = LeafNode(None, "Normal text")
@@ -17,6 +17,10 @@ def main():
     li3 = ParentNode("li", [leaf3])
     ul = ParentNode("ul", [li, li2, li3], {"background-color":"red"})
     
-    print(ul.to_html())
+    node = TextNode("This is text with a `code block` word", text_type_text)
+
+    new_nodes = split_nodes_delimiter([node, code], "`", text_type_code)
+
+    print(new_nodes)
 
 main()
---
+++ src/split_node_delimiter.py
@@ -0,0 +1,30 @@
+from textnode import text_type_bold,text_type_code,text_type_image, text_type_italic, text_type_link, text_type_text, TextNode
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    newList = []
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            newList.append(node)
+        else:
+            if delimiter not in node.text:
+                raise ValueError(f"Delimiter f{delimiter} was not found in text f{node.text}")
+            else:
+                words = node.text.split(delimiter)
+                for i in range(len(words)):
+                    if words[i] == "":
+                        continue
+                    if i % 2 != 0:
+                        print("entreaca ", words[i])
+                        newList.append(TextNode(words[i], text_type, node.url))
+                    else:
+                        print("entreaca else ", words[i])
+
+                        newList.append(TextNode(words[i], text_type_text, node.url))
+    
+
+    print("Inside function", newList)
+
+    return newList
+
+    
+        
